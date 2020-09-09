24bf66e44222d9fb11926fe627afcad570bddde5
Ezequiel Brilhante <ezequielrb7@gmail.com>
2020-09-09 01:38:57 -0300

    Conclusão do curso, ultimo ajustes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -27,6 +27,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
 import { ProductCreateComponent } from './components/product/product-create/product-create.component'
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
 import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component'
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 
 import localePt from '@angular/common/locales/pt';
@@ -44,7 +46,9 @@ registerLocaleData(localePt);
     ProductCrudComponent,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductRead2Component,
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -0,0 +1,37 @@
+import { Product } from './../product.module';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-update',
+  templateUrl: './product-update.component.html',
+  styleUrls: ['./product-update.component.scss'],
+})
+export class ProductUpdateComponent implements OnInit {
+  product: Product;
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.productService.readById(id).subscribe((product) => {
+      this.product = product;
+    });
+  }
+
+  updateProduct() {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso!');
+      this.router.navigate(['/products']);
+    });
+  }
+
+  cancel() {
+    this.router.navigate(['/products']);
+  }
+}
